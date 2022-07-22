c0ca4e3425c343190782b28b3e4b66de458023e2
Jabir Hussain <jabir-hussain@hotmail.co.uk>
2022-07-22 01:08:00 +0100

    Added date sorting

--- get_tweets.py
+++ get_tweets.py
@@ -6,9 +6,6 @@ except ModuleNotFoundError:
     !pip install snscrape
     !pip install pandas
 
-import snscrape.modules.twitter as scraper
-import pandas as pd
-
 def retrieve(accounts: List, phrase: "str") -> pd.DataFrame:
 
     data = []
@@ -18,8 +15,14 @@ def retrieve(accounts: List, phrase: "str") -> pd.DataFrame:
         for iteration, tweet in enumerate(scraper.TwitterSearchScraper(f'from:{account} {phrase}').get_items()):
             if iteration > 5:
                 break
-            data.append([account, tweet.date, tweet.content])
-
+            data.append([account, tweet.date.date(), tweet.content])
+    # make dataframe
     tweet_df = pd.DataFrame(data, columns=["Account Username", "Date Tweeted", "Tweet"])
+    # convert dates from object to datetime type
+    tweet_df["Date Tweeted"] = pd.to_datetime(tweet_df["Date Tweeted"])
+    # sort values in recency
+    tweet_df.sort_values(by="Date Tweeted", ascending=False, inplace=True)
     display(tweet_df)
-retrieve(["FabrizioRomano, David_Ornstein"])
+
+
+retrieve(["FabrizioRomano", "David_Ornstein", "NizaarKinsella"], "Chelsea")
