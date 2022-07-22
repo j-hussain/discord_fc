e60c0f81749aac5df0d75343019365a52acf5b68
Jabir Hussain <jabir-hussain@hotmail.co.uk>
2022-07-22 01:06:16 +0100

    Started working with Hikari

--- Bot.py
+++ Bot.py
@@ -1,3 +1,25 @@
 # Bot.py
 
+import hikari
+with open("../APIKeys/DFN_token.txt") as f:
+    api_token = f.read()
 
+bot = hikari.GatewayBot(token=api_token)
+
+@bot.listen()
+async def soap(event: hikari.GuildMessageCreateEvent) -> None:
+    # don't want bot to reply to itself
+    if event.is_bot or not event.content:
+        return
+
+    if.event.content.startswith("!soap"):
+        await event.message.respond("Soap? What kinda name is soap?")
+
+# async def transfer_request(event: hikari.GuildMessageCreateEvent) -> None:
+"""
+
+Implement actual interaction
+
+"""
+
+bot.run()
